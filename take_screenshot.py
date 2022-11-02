8cd8b2eebdf753b83ddbc30871cfbfb629419ef5
marcoProgamador123 <90981714+marcoProgamador123@users.noreply.github.com>
2022-11-02 13:45:19 -0300

    Update take_screenshot.py

--- take_screenshot.py
+++ take_screenshot.py
@@ -1,4 +1,3 @@
-from email.mime import image
 import numpy as np
 import pyautogui
 import imutils
@@ -46,7 +45,7 @@ while True:
 
              #verificando se todos os dedos estão dobrados
             if all(finger_fold_status):
-                image = pyautogui.screenshot()
+                image = pyautogui.screenshot("straight_to_disk.png")
                 image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR) 
                 cv2.imwrite("in_memory_to_disk.png",image)
             image = cv2.imread("straight_to_disk.png")
